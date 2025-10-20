a1792d3b01122e295e862e69ee57e7cd9e87e6e9
kiet-ta <trananhkiet21082005@gmail.com>
2025-10-20 11:34:44 +0700

    update main source by using recursion, to see green4ever

--- src/main/java/com/tak/mathutil/core/MathUtility.java
+++ src/main/java/com/tak/mathutil/core/MathUtility.java
@@ -13,20 +13,30 @@ public class MathUtility {
     //0,20 -> boundary - biên giới tập giá trị
     // xích qua 1 xíu là sang vùng invalid boundary!!!
     // 0, xích nhẹ sang trái 1 đơn vị, ko ổn, ko tính đc, -1 sao tính!!!
-    public static long getFactorial(int n)
-    {
-       if (n < 0 || n > 20) {
-           //ném ngoại lệ, kèm câu "chửi", và dừng hàm ngay, ko có value nào đc trả về !!!
-           throw new IllegalArgumentException("n must be between 0 and 20");
-       }
-
-       //biến trung gian để tính phép nhân
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i; // result = result * i // thuật toán heo đất, ốc bu nhồi thịt, vào đây tiếp chiêu
-
+//    public static long getFactorial(int n)
+//    {
+//       if (n < 0 || n > 20) {
+//           //ném ngoại lệ, kèm câu "chửi", và dừng hàm ngay, ko có value nào đc trả về !!!
+//           throw new IllegalArgumentException("n must be between 0 and 20");
+//       }
+//
+//       //biến trung gian để tính phép nhân
+//        long result = 1;
+//        for (int i = 1; i <= n; i++) {
+//            result *= i; // result = result * i // thuật toán heo đất, ốc bu nhồi thịt, vào đây tiếp chiêu
+//
+//        }
+//        return result;
+//
+//    }
+    //5! = 1.2.3.4.5 = 4!.5
+    public static  long getFactorial(int n){
+        if (n < 0 || n > 20)
+        {
+            throw new IllegalArgumentException("Bug gòi");
         }
-        return result;
-
+        if (n == 1 || n == 0) return 1;
+        return  n*getFactorial(n-1);
     }
+    // kiểm thử lại CODE ĐÃ TỐI ƯU. -REGRESSTION TESTING,TEST LẠI THỨ ĐÃ TỪNG TEST
 }
