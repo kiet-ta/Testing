c1057c07a73e08245f7265eb20b1d4caaa81d52d
kiet-ta <trananhkiet21082005@gmail.com>
2025-10-20 11:21:32 +0700

    upload source code with 20.10 version, to see red alert

--- src/test/java/com/tak/mathutil/core/MathUtilityDDTTest.java
+++ src/test/java/com/tak/mathutil/core/MathUtilityDDTTest.java
@@ -13,7 +13,7 @@ class MathUtilityDDTTest {
         Object [][] dataset = {
                 {5, 120},
                 {6, 720},
-                {4, 24},
+                {4, 240},
                 {2, 2},
                 {1, 1},
                 {0, 1}
